0058c2f3abfa80dce74bfe148e4d040328e78fae
Charaf Mrah <charaf4charaf@gmail.com>
2021-06-20 13:53:21 +0300

    Update eggOrChicken.java

--- eggOrChicken.java
+++ eggOrChicken.java
@@ -7,12 +7,12 @@ public class eggOrChicken {
 	public static void main(String[] args) {	
 		
 		String[] x = {"🥚", "🐓"};
+		
 		Arrays.sort(x);
-		for (int i = 0; i < x.length; i++) {
+		
+		for (int i = 0; i < x.length; i++) 
 			System.out.println(x[i]);
-		}
-		
-		
+
 	}
 
 }
